43239283f4ac308c56d7e2ec8838ab5b4b79b1b6
neervel <klihopoy@mail.ru>
2020-10-21 17:40:23 +0300

    animation for hidden cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
-document.getElementById("showMore").onclick = function(){
-  alert("А больше нет :(")
-}
+let hidden = true
+$("#showMore").click(function(){
+  if(hidden==true){
+    $("#hiddenCards").slideDown("normal");
+    $("#hiddenCards").css("display", "flex");
+    $(this).text("Показать меньше");
+    hidden = false;
+  }else{
+    $('html, body').animate({scrollTop: $(".works").offset().top}, 400);
+    $("#hiddenCards").fadeOut("normal");
+    $(this).text("Посмотреть ещё");
+    hidden = true;
+  }
+});
+
+$('#form').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 2
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    message: {
+      required: true
+    }
+  },
+  messages: {
+    name: {
+      required: "Укажите ваше имя",
+      minlength: "Имя должно быть не короче 2-х символов"
+    },
+    email: {
+      required: "Укажите ваш e-mail",
+      email: "Укажите корректный e-mail"
+    },
+    message: "Введите ваше сообщение"
+  }
+});
+$('#works').click(function(){
+  $('html, body').animate({scrollTop: $("#worksBlock").offset().top}, 500);
+  $("#worksBlock").addClass("androidFix").scrollTop(0).removeClass("androidFix");
+});
+$('#order').click(function(){
+  $('html, body').animate({scrollTop: $("#contactsBlock").offset().top}, 500);
+  $("#contactsBlock").addClass("androidFix").scrollTop(0).removeClass("androidFix");      
+  $("#nameInput").focus();
+});
+$(".works-card").hover(function(){
+  $(this).children(".works-card-info").toggleClass("hidden");
+  $(this).children(".works-card-info").toggleClass("visible");
+});
