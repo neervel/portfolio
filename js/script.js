d16c72213a20a2ebc314693980dd2afbee887eb3
neervel <klihopoy@mail.ru>
2021-02-15 16:56:59 +0300

    fix hidden block

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-let hidden = true
-$("#showMore").click(function(){
-  if(hidden==true){
-    $("#hiddenCards").slideDown("normal");
-    $("#hiddenCards").css("display", "flex");
-    $(this).text("Спрятать");
-    hidden = false;
-  }else{
-    $("#hiddenCards").slideUp("normal");
-    $(this).text("Посмотреть ещё");
-    hidden = true;
-    $('html, body').animate({scrollTop: $(".works-cards").offset().top}, 400);
+// let hidden = true
+// $("#showMore").click(function(){
+//   if(hidden==true){
+//     $("#hiddenCards").slideDown("normal");
+//     $(this).text("Спрятать");
+//     hidden = false;
+//   }else{
+//     $("#hiddenCards").slideUp("normal");
+//     hidden = true;
+//     $('html, body').animate({scrollTop: $(".works-cards").offset().top}, 400);
+//   }
+// });
+
+const moreBtn = $("#showMore"),
+      hiddenCards = $("#hiddenCards")
+moreBtn.on('click', function() {
+  if ( $(this).hasClass('is-active') ) {
+      $(this).removeClass('is-active');
+      hiddenCards.slideUp(300);
+      $('html, body').animate({scrollTop: $(".works-cards").offset().top}, 400);
+  } else {
+      $(this).addClass('is-active');
+      hiddenCards.slideDown(400);
   }
 });
 
@@ -42,11 +53,6 @@ $('#form').validate({
 $('#works').click(function(){
   $('html, body').animate({scrollTop: $("#worksBlock").offset().top}, 500);
   $("#worksBlock").addClass("androidFix").scrollTop(0).removeClass("androidFix");
-});
-$('#order').click(function(){
-  $('html, body').animate({scrollTop: $("#contactsBlock").offset().top}, 500);
-  $("#contactsBlock").addClass("androidFix").scrollTop(0).removeClass("androidFix");      
-  $("#nameInput").focus();
 });
 $(".works-card").hover(function(){
   $(this).children(".works-card-info").toggleClass("hidden");
